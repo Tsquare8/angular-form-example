e68b4ae247b4e15ba142123ccec359ee761bb26a
Tate n0112138 <Terri.Tate@LibertyMutual.com>
2017-07-05 10:01:57 -0400

    add FormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms' //add to use form
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+//add method below 
+onSubmit(data: NgForm){
+  console.log(data.value)
+}
 }
